9ac0e8b0316b796cd164d37d637a3e03e0be51bd
2bWant2b <tzh363231879@163.com>
2021-04-23 13:54:56 +0800

    加入判断括号合法性

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,15 +1,28 @@
 #include <cstdio>
 #include <malloc.h>
+#include <string.h>
 #define MaxSize 100
 typedef int ElemType;
+typedef char ElemType2;
 typedef struct 
 {
     ElemType data[MaxSize];
     int top;
 } SqStack;
+typedef struct 
+{
+    ElemType2 data[MaxSize];
+    int top;
+} SqStack2;
+
 
 void initStack(SqStack*&s) {
 	s = (SqStack*)malloc(sizeof(SqStack));
+	s->top = -1;
+}
+
+void initStack2(SqStack2 *&s) {
+	s = (SqStack2 *)malloc(sizeof(SqStack2));
 	s->top = -1;
 }
 
@@ -21,6 +34,10 @@ bool isEmpty(SqStack*s){
 	return(s->top == -1);
 }
 
+bool isEmpty2(SqStack2 *s){
+    return(s->top == -1);
+}
+
 bool push(SqStack*&s, ElemType e) {
 	if (s->top==MaxSize-1)
         return false;
@@ -29,7 +46,15 @@ bool push(SqStack*&s, ElemType e) {
 	return true;
 }
 
-bool pop(SqStack*&s, ElemType&e) {
+bool push2(SqStack2 *&s, ElemType2 e) {
+	if (s->top==MaxSize-1)
+        return false;
+	s->top++;
+	s->data[s->top]=e;
+	return true;
+}
+
+bool pop(SqStack*&s, ElemType &e) {
 	if (s->top == -1)
 		return false;
 	e = s->data[s->top];
@@ -37,10 +62,63 @@ bool pop(SqStack*&s, ElemType&e) {
 	return true;
 }
 
-int peek(SqStack*s) {
+bool pop2(SqStack2 *&s, ElemType2 &e){
+    if (s->top == -1)
+		return false;
+	e = s->data[s->top];
+	s->top--;
+	return true;
+}
+
+char popElem(SqStack2 *&s){
+    if (s->top == -1)
+		return false;
+	return s->data[s->top];
+	s->top--;
+}
+
+int peek(SqStack *s) {
 	if (s->top == -1)
 		return false;
 	return s->data[s->top];
+}
+
+char peek2(SqStack2 *s){
+    if (s->top == -1)
+		return false;
+	return s->data[s->top];
+}
+
+bool judgeBrackets(char str[]){
+    char *p=str;
+    SqStack2 *s;
+    ElemType2 e;
+    s = (SqStack2 *)malloc(sizeof(SqStack2));
+	s->top = -1;
+    for(int i=0;i<strlen(str);i++){
+        if(p[i]!=')'&&p[i]!=']'&&p[i]!='}'&&p[i]!='('&&p[i]!='['&&p[i]!='{'){
+            continue;
+        }
+        else if(p[i]=='('){
+            push2(s,')');
+        }
+        else if(p[i]=='['){
+            push2(s,']');
+        }
+        else if(p[i]=='{'){
+            push2(s,'}');
+        }
+        else if(p[i]==peek2(s)){
+            pop2(s,e);
+        }
+        else if(isEmpty2(s)||p[i]!=peek2(s)){
+            return false;
+        }
+    }
+    if(isEmpty2(s)){
+        return true;
+    }
+    return false;
 }
 
 int main() {
@@ -64,24 +142,9 @@ int main() {
     printf("\n");
     printf("Stack is %s\n",(isEmpty(s)?"empty":"not empty"));
     destoryStack(s);
+    printf("---------------additional question-----------------\n");
+    char str1[]="a*{b*[1/(2-c)+1/(1+d)]-3}";
+    char str2[]="a*{b*1/(2-c)+1/(1+d)]-3";
+    printf("%s\n",(judgeBrackets(str1)?"valid brackets":"invalid brackets"));
+    printf("%s\n",(judgeBrackets(str2)?"valid brackets":"invalid brackets"));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
